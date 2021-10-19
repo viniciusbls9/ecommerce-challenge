7f6962ed2e31f70b8f67647a87cb4a5c2059fa62
Vinicius <beneditoloura@gmail.com>
2021-10-18 23:00:28 -0300

    ♻️ refactor: refactor jest config and storybook

--- jest.config.js
+++ jest.config.js
@@ -3,5 +3,7 @@ module.exports = {
   testPathIgnorePatterns: ['/node_modules/', '/.next/'],
   collectCoverage: true,
   collectCoverageFrom: ['src/**/*.ts(x)?', '!src/**/stories.tsx'],
-  setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts']
+  setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
+  moduleDirectories: ['node_modules', 'src'],
+  modulePathIgnorePatterns: ['<rootDir>/src/types', '<rootDir>/src/pages/_app.tsx']
 }
