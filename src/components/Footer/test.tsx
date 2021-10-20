af9f3f72c58d43ec0877efd5c26f30cc8adec7a0
Vinicius Benedito <beneditoloura@gmail.com>
2021-10-20 16:38:01 -0300

    ✅ test: create first test and snapshot

---
+++ src/components/Footer/test.tsx
@@ -0,0 +1,11 @@
+import { renderWithTheme } from 'utils/helpers'
+
+import Footer from '.'
+
+describe('<Footer />', () => {
+  it('should render the heading', () => {
+    const { container } = renderWithTheme(<Footer />)
+
+    expect(container.firstChild).toMatchSnapshot()
+  })
+})
