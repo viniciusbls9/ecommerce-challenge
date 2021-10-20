462bbb0479bc39ae214456e820b7a3f9bb12ccff
Vinicius <beneditoloura@gmail.com>
2021-10-19 21:59:33 -0300

    ✨ feat: NavigationBar component finished

--- src/components/NavigationBar/index.tsx
+++ src/components/NavigationBar/index.tsx
@@ -1,9 +1,22 @@
 import * as S from './styles'
+import mockNavigation from 'mocks/mock-navigation-bar.json'
 
-const NavigationBar = () => (
-  <S.Wrapper>
-    <h1>NavigationBar</h1>
-  </S.Wrapper>
-)
+const NavigationBar = () => {
+  return (
+    <S.Wrapper>
+      <S.Container
+        className="navigation-bar-container"
+        repeatCount={mockNavigation.length}
+      >
+        {mockNavigation.map((items) => (
+          <S.SingleNavigationContainer key={items.iconName}>
+            <S.NavigationIconImg src={items.iconPath} />
+            <S.NavigationName>{items.iconName}</S.NavigationName>
+          </S.SingleNavigationContainer>
+        ))}
+      </S.Container>
+    </S.Wrapper>
+  )
+}
 
 export default NavigationBar
--- src/components/NavigationBar/styles.ts
+++ src/components/NavigationBar/styles.ts
@@ -1,3 +1,54 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
+import media from 'styled-media-query'
 
-export const Wrapper = styled.main``
+export const Wrapper = styled.section`
+  width: 100%;
+
+  position: absolute;
+  bottom: 0;
+
+  ${media.greaterThan('medium')`
+    position: initial;
+  `}
+`
+
+interface ContainerProps {
+  repeatCount: number
+}
+
+export const Container = styled.div<ContainerProps>`
+  ${({ repeatCount }) => css`
+    width: 100%;
+    max-width: 1050px;
+    margin: 0 auto;
+    padding: 5px;
+    display: grid;
+    grid-template-columns: repeat(${repeatCount}, 1fr);
+    gap: 10px;
+
+    box-shadow: 0px -3px 6px #00000029;
+  `}
+`
+
+export const SingleNavigationContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+
+  &:last-child {
+    width: 70px;
+  }
+`
+export const NavigationIconImg = styled.img`
+  width: 18px;
+  height: 18px;
+`
+export const NavigationName = styled.h5`
+  ${({ theme }) => css`
+    color: ${theme.colors.gray};
+    font-family: ${theme.font.family};
+    /* font-size: ${theme.font.sizes.xsmall}; */
+  `}
+`
