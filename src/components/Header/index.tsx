337a2ffc16e38cd6c50905c04cffc41a301dd4b5
Vinicius <beneditoloura@gmail.com>
2021-10-19 21:57:43 -0300

    ✨ feat: include NavigationBar in Headee

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,16 @@
 import * as S from './styles'
 import Logo from '../Logo'
+import NavigationBar from '../NavigationBar'
 
 const Header = () => (
-  <S.Wrapper>
-    <S.Container>
-      <Logo />
-    </S.Container>
-  </S.Wrapper>
+  <>
+    <S.Wrapper>
+      <S.Container>
+        <Logo />
+      </S.Container>
+    </S.Wrapper>
+    <NavigationBar />
+  </>
 )
 
 export default Header
