26cb5ac0186cb91d263568016e4d7c504880865c
Vinicius <beneditoloura@gmail.com>
2021-10-19 22:00:05 -0300

    ✅ test: ensure render navigationBar with correctly values

--- src/components/NavigationBar/test.tsx
+++ src/components/NavigationBar/test.tsx
@@ -1,15 +1,28 @@
-import { render, screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/helpers'
+import mockNavigation from 'mocks/mock-navigation-bar.json'
 
 import NavigationBar from '.'
 
 describe('<NavigationBar />', () => {
-  it('should render the heading', () => {
-    const { container } = render(<NavigationBar />)
+  it('should render the NavigationBar for mobile', () => {
+    const { container } = renderWithTheme(<NavigationBar />)
+
+    expect(container.firstChild).toHaveStyleRule('bottom', '0')
+  })
+
+  it('should render the NavigationBar for desktop', () => {
+    const { container } = renderWithTheme(<NavigationBar />)
+
+    expect(container.firstChild).toHaveStyleRule('position', 'initial', {
+      media: '(min-width: 768px)'
+    })
+  })
+
+  it('should render the NavigationBar container with repeatCount prop value', () => {
+    const { container } = renderWithTheme(<NavigationBar />)
 
     expect(
-      screen.getByRole('heading', { name: /NavigationBar/i })
-    ).toBeInTheDocument()
-
-    expect(container.firstChild).toMatchSnapshot()
+      container.querySelector('.navigation-bar-container')?.childElementCount
+    ).toBe(mockNavigation.length)
   })
 })
