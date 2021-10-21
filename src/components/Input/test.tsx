797c38f1222a8df637017b7b6cabb213ba573322
Vinicius <beneditoloura@gmail.com>
2021-10-20 21:04:47 -0300

    ✅ test: create snapshot

--- src/components/Input/test.tsx
+++ src/components/Input/test.tsx
@@ -1,12 +1,10 @@
-import { render, screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/helpers'
 
 import Input from '.'
 
 describe('<Input />', () => {
-  it('should render the heading', () => {
-    const { container } = render(<Input />)
-
-    expect(screen.getByRole('heading', { name: /Input/i })).toBeInTheDocument()
+  it('should render the Input', () => {
+    const { container } = renderWithTheme(<Input />)
 
     expect(container.firstChild).toMatchSnapshot()
   })
