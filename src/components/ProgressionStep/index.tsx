8cf1bd12f6196b6a4d6a1cf5a8664b20e1129a41
Vinicius <beneditoloura@gmail.com>
2021-10-20 22:07:00 -0300

    feat: initial code

---
+++ src/components/ProgressionStep/index.tsx
@@ -0,0 +1,9 @@
+import * as S from './styles'
+
+const ProgressionStep = () => (
+  <S.ProgressWrapper value="50" max="100">
+    <h1>ProgressionStep</h1>
+  </S.ProgressWrapper>
+)
+
+export default ProgressionStep
---
+++ src/components/ProgressionStep/stories.tsx
@@ -0,0 +1,9 @@
+import { Story, Meta } from '@storybook/react/types-6-0'
+import ProgressionStep from '.'
+
+export default {
+  title: 'ProgressionStep',
+  component: ProgressionStep
+} as Meta
+
+export const Default: Story = () => <ProgressionStep />
