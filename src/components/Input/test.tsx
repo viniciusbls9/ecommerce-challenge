ce4a968008b7537fdf9f81e5fb2f5db23803da13
Vinicius Benedito <beneditoloura@gmail.com>
2021-10-27 07:40:41 -0300

    ✅ test: ensure render input with select type

--- src/components/Input/test.tsx
+++ src/components/Input/test.tsx
@@ -1,11 +1,20 @@
 import { renderWithTheme } from 'utils/helpers'
+import { screen } from '@testing-library/react'
 
 import Input from '.'
 
+const props = ['menor preço']
+
 describe('<Input />', () => {
   it('should render the Input', () => {
-    const { container } = renderWithTheme(<Input />)
+    const { container } = renderWithTheme(<Input type="text" />)
 
     expect(container.firstChild).toMatchSnapshot()
+  })
+
+  it('should render the Input with select type', () => {
+    renderWithTheme(<Input type="select">{props}</Input>)
+
+    expect(screen.getByRole('option', { name: 'menor preço' }))
   })
 })
