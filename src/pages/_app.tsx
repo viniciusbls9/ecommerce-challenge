c9c6f7a3235896fa497e17439013ca8265a8e61b
Vinicius Benedito <beneditoloura@gmail.com>
2021-10-28 08:42:41 -0300

    ⚰️ refactor: remove unused code

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -14,23 +14,13 @@ function App({ Component, pageProps }: AppProps) {
       <GlobalStyles />
       <Head>
         <title>Ecommerce</title>
-        <link
-          rel="shortcut icon"
-          href="/img/icon-512.png"
-          type="image/x-icon"
-        />
+
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
           href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;500;700&display=swap"
           rel="stylesheet"
         />
-        <link
-          rel="apple-touch-icon"
-          href="/img/icon-512.png"
-          type="image/x-icon"
-        />
-        <link rel="manifest" href="/manifest.json" />
         <meta
           name="description"
           content="A simple proct starter to work with Typescript, React, NextJS and Styled Components"
