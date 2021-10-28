fae12b5edcb33a290cfde7947a6d46cdd4f6b179
Vinicius Benedito <beneditoloura@gmail.com>
2021-10-28 08:40:39 -0300

    ⚰️ refactor: remove unused code

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -3,24 +3,9 @@ import * as S from './styles'
 
 export interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   type?: string
-  children?: string[]
 }
 
-const Input: React.FC<InputProps> = ({
-  type,
-  children,
-  ...rest
-}: InputProps) => {
-  if (type === 'select') {
-    return (
-      <S.InputSelect>
-        {children?.map((item) => (
-          <S.Options key={item}>{item}</S.Options>
-        ))}
-      </S.InputSelect>
-    )
-  }
-
+const Input: React.FC<InputProps> = ({ type, ...rest }: InputProps) => {
   return (
     <S.InputWrapper>
       <S.Input type={type} {...rest} />
