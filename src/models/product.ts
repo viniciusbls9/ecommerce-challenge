98b0925f722ac771aa926e75f5781498682ef264
Vinicius <beneditoloura@gmail.com>
2021-10-27 22:18:09 -0300

    ✨ feat: create models and service fakeAPI

---
+++ src/models/product.ts
@@ -0,0 +1,12 @@
+export interface SingleProductProps {
+  id: number
+  image: string
+  title: string
+  price: string
+  category: string
+  description: string
+}
+
+export interface ProductProps {
+  products?: SingleProductProps[]
+}
---
+++ src/services/fakeAPI.ts
@@ -0,0 +1,13 @@
+import axios from 'axios'
+import { SingleProductProps } from 'models/product'
+
+const BASE_URL = 'https://fakestoreapi.com'
+
+export default {
+  getRecommendedProducts: async (limit?: string) => {
+    const req = await axios.get<SingleProductProps>(
+      `${BASE_URL}/products?limit=${limit}`
+    )
+    return req.data
+  }
+}
