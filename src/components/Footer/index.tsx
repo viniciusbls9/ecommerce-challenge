ab9ec698ef7a832c57245e13c80df5aac2d6890c
Vinicius Benedito <beneditoloura@gmail.com>
2021-11-05 06:39:25 -0300

    🎨 update: change format code

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -2,18 +2,22 @@ import mockNavigation from 'mocks/mock-footer.json'
 import Link from 'next/link'
 import * as S from './styles'
 
-const Footer = () => (
-  <S.FooterWrapper>
-    <S.FooterContainer>
-      {mockNavigation.map((items) => (
-        <Link href={items.redirect} key={items.redirect}>
-          <S.SingleIconContainer>
-            <S.NavigationIconImg src={items.iconPath} />
-          </S.SingleIconContainer>
-        </Link>
-      ))}
-    </S.FooterContainer>
-  </S.FooterWrapper>
-)
+const Footer = () => {
+  return (
+    <S.FooterWrapper>
+      <S.FooterContainer>
+        {mockNavigation.map((items) => {
+          return (
+            <Link href={items.redirect} key={items.redirect}>
+              <S.SingleIconContainer>
+                <S.NavigationIconImg src={items.iconPath} />
+              </S.SingleIconContainer>
+            </Link>
+          )
+        })}
+      </S.FooterContainer>
+    </S.FooterWrapper>
+  )
+}
 
 export default Footer
